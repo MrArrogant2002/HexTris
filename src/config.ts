27a79027e6aff426d491860682ff3e54ae226d96
Rapolu Eswara Balu <159517779+MrArrogant2002@users.noreply.github.com>
2026-02-16 20:54:36 +0530

    Clear Appwrite configuration values

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 ﻿// Appwrite Configuration
 (window as any).appwriteConfig = {
-  endpoint: 'https://fra.cloud.appwrite.io/v1',
-  projectId: '69909628003aaf679156',
-  databaseId: '6984b02b00220847a944',  // Update this after creating the database
-  usersCollectionId: 'users'  // Update this after creating the collection
+  endpoint: '',
+  projectId: '',
+  databaseId: '',  // Update this after creating the database
+  usersCollectionId: ''  // Update this after creating the collection
 };
 
 // Server Configuration
